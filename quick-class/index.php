ec965cfa212e874e78b233563eca7af64aa92145
Juciellen <jucarol17@gmail.com>
2017-12-01 23:51:52 -0200

    files of quick class

---
+++ quick-class/index.php
@@ -0,0 +1,29 @@
+<?php
+
+// $nome = "Daniel";
+// echo "olá $nome";
+// echo "<br>";
+// var_dump($nome);
+// echo "<br>";
+// $nome = 527;
+// var_dump($nome);
+// echo "<br>";
+// $nome = 1.5;
+// var_dump($nome);
+// echo "<br>";
+// $nome = null;
+// var_dump($nome);
+// echo "<br>";
+// $nome = false;
+// var_dump($nome);
+// echo "<br>";
+
+
+// $nome = 'PHP';
+// print_r($nome);
+// echo "<br>";
+// var_dump($nome);
+echo "<pre>";
+$familia = ["Yasmin","Juciellen","Rafael"];
+print_r($familia);
+var_dump($familia);
