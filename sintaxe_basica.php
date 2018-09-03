d283733546ba89b86ffda3dadcdc19200582828a
juciellen.cabrera@4linux.com.br <juciellen.cabrera@4linux.com.br>
2018-09-03 20:29:31 -0300

    sintavxe basica, variaveis e incremento

---
+++ sintaxe_basica.php
@@ -0,0 +1,9 @@
+<h1>Desenvolvimento Web com PHP</h1>
+<?php 
+	$nome = 'Juciellen';
+	echo '<h1>Bem vindo ao curso de PHP $nome!</h1>';
+?>
+
+<h1>Bem vindo ao curso de PHP <?=$nome;?>!</h1>
+<h1>Bem vindo ao curso de PHP <?php echo $nome;?>!</h1>
+
