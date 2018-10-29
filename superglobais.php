10e534d66da190d8080cfbecc5bb289df08449ec
juciellen.cabrera@4linux.com.br <juciellen.cabrera@4linux.com.br>
2018-10-29 21:21:31 -0200

    SOAP

--- superglobais.php
+++ superglobais.php
@@ -1,7 +1,12 @@
 <?php
-echo "<pre>";
-$nome='Juciellen';
-// print_r($_SERVER);
+
+print_r('PHP_SELF '.$_SERVER['PHP_SELF']);
+echo PHP_EOL."<br>";
+print_r('REQUEST_URI ' .$_SERVER['REQUEST_URI']);
+echo PHP_EOL."<br>";
+
+// echo "<pre>";
+// $nome='Juciellen';
 // print_r($GLOBALS);
 // $_SESSION
 // $GLOBALS
