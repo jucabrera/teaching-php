bb353f8586b25bd08c8c1a5c6bf6b68cd55533c6
Juciellen Cabrera <jucarol17@gmail.com>
2019-03-20 20:49:38 -0300

    modificacão de arquivo de sitaxe basica e hello world

--- sintaxe_basica.php
+++ sintaxe_basica.php
@@ -1,7 +1,8 @@
+<h1>4500</h1>
 <h1>Desenvolvimento Web com PHP</h1>
 <?php 
 	$nome = 'Juciellen';
-	echo '<h1>Bem vindo ao curso de PHP $nome!</h1>';
+	echo '<h1>Bem vindo ao curso EAD de PHP $nome!</h1>';
 ?>
 
 <h1>Bem vindo ao curso de PHP <?=$nome;?>!</h1>
